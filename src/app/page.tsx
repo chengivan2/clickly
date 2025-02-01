12eaabecdf61109c787402ed75b78d9f795af053
chengivan <chengoleivan@gmail.com>
2025-02-01 21:25:37 +0300

    Added link to sign-up in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
       <main className={styles.main}>
+
+        <h2>Welcome to Clickly</h2>
+        <div className="auth-links">
+          <Link href="./sign-up">Sign Up</Link>
+        </div>
 
       </main>
   );
